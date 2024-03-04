7eee485eabee7b9f5a1715af3546e9676781f0ef
TheGoodSpirit <mannish079@gmail.com>
2024-03-04 07:05:54 +0545

    new program added in OOP

--- Object Oriented Programming in Java/src/com/Run_Main/Run_Main.java
+++ Object Oriented Programming in Java/src/com/Run_Main/Run_Main.java
@@ -1,29 +1,59 @@
 package com.Run_Main;
+import java.util.Scanner;
 
 // importing other packages
 import com.greet.*;
 import com.simple_Interest.*;
 import com.method_overloading.*;
 import com.method_overriding.*;
+import com.simple_Interest.*;
+import com.Area.*;
 
 public class Run_Main {
 	public static void main(String[] args) {
-
-		// Greet Program
-		Greet _msg = new Greet("Hello World!");
 		
-		// Calculating Simple Interest
-		SimpleInterest _SI = new SimpleInterest(10000, 20, 15.4f);
+		// Scanner initialization
+		Scanner scan = new Scanner(System.in);
 		
-		// Method Overloading
-		Method_Overloading _overloading1 = new Method_Overloading("Overloading.......");
-		Method_Overloading _overloading2 = new Method_Overloading(405);
+		System.out.println("========== OPTION ==========");
+		System.out.println("1. Simple Interest");
+		System.out.println("2. Area\n");
 		
-		// Method Overriding
-		Bike honda = new Bike();
-		honda.engine();
 		
-		Car benz = new Car();
-		benz.engine();
+		// option
+		System.out.print("\nEnter the option : ");
+		int op = scan.nextInt();
+		
+		switch(op) {
+			case 1:
+				// ==================== Simple Interest ====================
+				System.out.println("\n========== Simple Interest ==========");
+				// for principal
+				System.out.print("Enter the principal: ");
+				int p = scan.nextInt();
+				// for time
+				System.out.print("Enter the time: ");
+				int t = scan.nextInt();
+				// for rate
+				System.out.print("Enter the rate: ");
+				float r = scan.nextFloat();
+				SimpleInterest _SI = new SimpleInterest(p, t, r);
+				break;
+			
+			case 2:
+				// ==================== Area ====================
+				System.out.println("\n========== Area ==========");
+				System.out.print("Enter the length: ");
+				float l = scan.nextFloat();
+				System.out.print("Enter the breadth: ");
+				float b = scan.nextFloat();
+				
+				// Creating object for class Area
+				Area area = new Area(l,b);
+				area.check();
+				area.calculate();
+				area.display();
+				break;
+		}
 	}
 }
